f45fbd9c8507dc5fd268cb90c6dd052161fe62d5
himel1625 <himelmia1625@gmail.com>
2025-02-27 23:57:49 +0600

    utilize the project sculpture and it's now ready to implement

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className=' flex items-center justify-center mt-20'>
+      <h1 className='text-3xl font-bold text-blue-600'>Redux Todo App</h1>
+      
+      <div></div>
+    </div>
+  );
+};
+
+export default App;
